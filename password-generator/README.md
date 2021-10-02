63a4f342e6d1a8007c61bafd373237cbca003706
Weston Knowlton <kno18012@byui.edu>
2021-10-02 13:27:49 -0600

    filled out readme.md and added code comments

--- password-generator/README.md
+++ password-generator/README.md
@@ -1,30 +1,30 @@
 # Overview
 
-{Important!  Do not say in this section that this is college assignment.  Talk about what you are trying to accomplish as a software engineer to further your learning.}
-
-{Provide a description the software that you wrote to demonstrate the Python language.}
-
-{Describe your purpose for writing this software.}
+This is a secure password generator created in Python. The purpose of this program is to give the user a way to easily create secure passwords for use in their lives. This program is like others that you see on the internet but 
+here to have the comfort o knowing that all of the generation is done locally on your computer, nothing behind the scenes. In this program I use a variety of different methods to accomplish my goal. This could have been done 
+more simply but I also wanted to exhibit my understanding of the different ways to solve a problem in Python. 
 
 {Provide a link to your YouTube demonstration.  It should be a 4-5 minute demo of the software running and a walkthrough of the code.  Focus should be on sharing what you learned about the language syntax.}
 
-[Software Demo Video](http://youtube.link.goes.here)
+[Password Generator - Python Demo Video](https://youtu.be/0kHeoyhKIxg)
 
 # Development Environment
 
-{Describe the tools that you used to develop the software}
+Visual Studio Code
 
-{Describe the programming language that you used and any libraries.}
-
+Python 3
+* Random library
+* Miscellaneous operating system interfaces library(os)
+ 
 # Useful Websites
 
-{Make a list of websites that you found helpful in this project}
-* [Web Site Name](http://url.link.goes.here)
-* [Web Site Name](http://url.link.goes.here)
+* [Python](https://www.python.org/)
+* [W3Schools](https://www.w3schools.com/python/default.asp)
+* [Stack Overflow](https://stackoverflow.com/)
+
 
 # Future Work
 
-{Make a list of things that you need to fix, improve, and add in the future.}
-* Item 1
-* Item 2
-* Item 3
+* Add functionality to label your passwords for the sites they will be used on
+* Add the ability to update the passwords file instead of rewriting it each time
+* Add functionality to remember your setting for the next time you need new passwords
--- password-generator/pwd-gen.py
+++ password-generator/pwd-gen.py
@@ -1,21 +1,39 @@
+# The random library allows this program to find random characters from a list of characters
 import random
+# The os library is the miscellaneous operating system interfaces library. It covers a lot of 
+# operating system functionality and is used in this program to separate passwords in a file by 
+# a blank line.
 import os
 
+# Initiates a class called Password to hold all of the password generator code
 class Password:
+    # The __init__() function is used to create a new instance of the Password class. 
+    # To create an instance of the Password class it needs three variables to be passed in,
+    # default_characters, run (true or false), and a file
     def __init__(self, default_characters, run, file):
         self.run = run
         self.default_characters = default_characters
         self.file = file
 
+    # The pwd_generator() method is the function that is called to create the passwords. It is passed the 
+    # self parameter which allows it to access the variables defined in the __init__() function
     def pwd_generator(self):
+        # While loop runs until run is set equal to False
         while self.run:
+            # Gets password length and number of passwords from the user
             pwd_length = int(input("How long would you like your secure password to be?: "))
             pwd_count = int(input("How many passwords would you like to generate?: "))
+            # Sets a variable password_list to hold all of the created passwords
             password_list = []
 
-            custom_chars_choice = input("Would you like to use a default list of characters? (alternative - you enter what characters you want in your passwords)(y or n): ")
+            # Gets users choice to use default characters or input their own
+            custom_chars_choice = input("Would you like to use a default list of characters? (alternative - you enter what characters you want in your passwords) (y or n): ")
 
+            # If Elif statement executes 2 different password generation processes based on their answer to the question about
             if custom_chars_choice.capitalize() == "Y":
+                # Loops through the pwd_count and pwd_length based on the values given above. password_char is chosen randomly 
+                # from default_characters and combind to make one password. That password it appended to the password_list.
+                # Run is set to False once all of the passwords have been generated to end the loop.
                 for x in range(0, pwd_count):
                     password = ""
                     for x in range(0, pwd_length):
@@ -24,10 +42,14 @@ class Password:
                     password_list.append(password)
                     self.run = False
             elif custom_chars_choice.capitalize() == "N":
+                # The user enters a string of desired characters without any spaces between them.
                 user_input = input("What characters would you like to include in your password? (enter them all in one line and without any spaces): ")
 
+                # The string of characters is looped through and converted to an array 
                 custom_chars = [char for char in user_input]
 
+                # This for loops does the same thing as the one above only using the custom_chars instead of the 
+                # default_chars
                 for x in range(0, pwd_count):
                     password = ""
                     for x in range(0, pwd_length):
@@ -36,15 +58,22 @@ class Password:
                     password_list.append(password)
                     self.run = False
 
+        # Opens the file that was passed in when the class was initialized and prepares to write to it. 
+        # The list of passwords, password_list, is converted into an enumerated object, [(0, password1), (1,password2), ...]
+        # the index + 1 and the password are written to the file. os.linesep inserts a new line after each password for readability
         with open(self.file, 'w') as f:
             for idx, password in enumerate(password_list):
                 f.write(f"{idx + 1}) {password} {os.linesep}")
 
+# Declaration of 2 files. One for the default characters the other for the finished password list
 file1 = "passwords.txt"
 file2 = "random_chars.txt"
 
+# Opens and reads the random_chars.txt file and converts the contents into a Python list
 with open(file2, 'r') as f:
     default_characters = [char for char in f.read()]
 
+# Initializes a new instance of the Password class passing in the 3 variables required
 passwords = Password(default_characters, True, file1)
+# Calls the pwd_generator() method in the Password class
 passwords.pwd_generator()
