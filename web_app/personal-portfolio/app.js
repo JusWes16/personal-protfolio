e588eca520529f8def81314568e5d00b5e9748fe
Weston Knowlton <kno18012@byui.edu>
2021-10-17 21:37:00 -0600

    removed console.log's

--- web_app/personal-portfolio/app.js
+++ web_app/personal-portfolio/app.js
@@ -46,7 +46,6 @@ const projects =  [
 
 
 app.get('/', (req, res) => {
-    console.log('Home Page');
     res.render('index', {
         pageTitle: 'Portfolio | Home',
         path: '/',
@@ -55,7 +54,6 @@ app.get('/', (req, res) => {
 });
 
 app.get('/projects', (req, res) => {
-    console.log('Projects Page');
     res.render('projects', {
         pageTitle: 'Portfolio | Projects',
         path: '/projects',
@@ -65,7 +63,6 @@ app.get('/projects', (req, res) => {
 });
 
 app.post('/projects-searched', (req, res) => {
-    console.log('Projects Page');
     const search = req.body.searched;
 
     let searched_projects = [];
@@ -85,7 +82,6 @@ app.post('/projects-searched', (req, res) => {
 });
 
 app.get('/about', (req, res) => {
-    console.log('About Page');
     res.render('about', {
         pageTitle: 'Portfolio | About',
         path: '/about',
