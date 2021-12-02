0c8c8e3e709743ddde4d10aa1f587e9b2f9b3c35
Weston Knowlton <kno18012@byui.edu>
2021-12-01 22:53:56 -0700

    module 5, lotr database project

---
+++ lort_data/README.md
@@ -0,0 +1,41 @@
+# Overview
+
+The software that I wrote here is a little demonstration of how to use SQL along with Python to perform CRUD operations to a database. The database that I have created and 
+that users are interacting with is a database of Lord of the Rings characters. This is a simple program that asked for user input to know what operations to perform. By entering 
+a value between 1 and 5 you can perform different CRUD operations to the database. Entering 6 will exit the program and anything other than 1 through 6 will prompt and error message.
+
+The purpose of this program really is to demonstrate how to use SQL and basic Python the create a simple, easy to use database system. Although the way I have written this program 
+is based around The Lord of the Rings, it can be implemented for any database for keeping track of individual people, for example, members of a family, members of a club or team, 
+or tenants at an apartment complex.
+
+{Provide a link to your YouTube demonstration.  It should be a 4-5 minute demo of the software running, a walkthrough of the code, and a view of how created the Relational Database.}
+
+[Software Demo Video](http://youtube.link.goes.here)
+
+# Relational Database
+
+I am using a simple one table database using Python and SQLite3 to store and access values from a local file.
+
+The structure is very simple. There is only one table which can hold any number of character data. Each entry requires a name, a race, the characters current life status, and a url for 
+an image of the character. Each of these values has a data type of text. 
+
+# Development Environment
+
+VSCode
+
+Python 
+SQLite3
+
+# Useful Websites
+
+* [Python Geeks](https://pythongeeks.org/)
+* [YouTube](https://www.youtube.com/watch?v=pd-0G0MigUA&ab_channel=CoreySchafer)
+* [Stack Overflow](https://stackoverflow.com/)
+* [Python](https://www.python.org/doc/)
+* [W3Schools](https://www.w3schools.com/python/)
+
+# Future Work
+
+* Expand the database to include more than one table 
+* Add more functionality for the user to make changes to the database
+* Add some sort of GUI to improve the user experience
---
+++ lort_data/lotr_characters.py
@@ -0,0 +1,100 @@
+import sqlite3
+conn = sqlite3.connect('lotr.db')
+cur = conn.cursor()
+
+# cur.execute(
+#     """
+#         CREATE TABLE characters(
+#             name text,
+#             race text,
+#             life_status text,
+#             image_url text
+#         )
+#     """)
+
+# c.execute("INSERT INTO characters VALUES ('Gandalf', 'Istar (Wizard)', 'alive', 'https://static.wikia.nocookie.net/lotr/images/e/e7/Gandalf_the_Grey.jpg/revision/latest?cb=20121110131754')")
+# c.execute("INSERT INTO characters VALUES ('Aragorn', 'Man (Dunedain)', 'dead', 'https://static.wikia.nocookie.net/lotr/images/b/b6/Aragorn_profile.jpg/revision/latest?cb=20170121121423')")
+
+def add_character():
+    name = input('Please enter the characters name: ').lower()
+    race = input('Please enter the race of this character: ').lower()
+    status = input('Please enter whether this character is alive, dead, or unknown: ').lower()
+    image_url = input('Please add an image URL if you have one: ').lower()
+
+    with conn:
+        cur.execute("INSERT INTO characters VALUES (:name, :race, :status, :image_url)", {'name': name, 'race': race, 'status': status, 'image_url': image_url})
+
+    print(f'{name} was sucessfully added!')
+
+def remove_character():
+    name = input('Please enter the characters name that you would like to delete: ').lower()
+
+    with conn:
+        cur.execute("DELETE FROM characters WHERE name = :name", {'name': name})
+
+    print(f'{name} was sucessfully deleted.')
+
+def update_character_life_status():
+    name = input('Please enter the name of the character: ').lower()
+    status = input('Please enter this characters new status: alive, dead, or unknown: ').lower()
+
+    with conn:
+        cur.execute("UPDATE characters SET life_status = :status WHERE name= :name", {'name': name, 'status': status})
+
+    print(f"{name}'s life staus was sucessfully updated to '{status}''.")
+
+def get_characters():
+    cur.execute("SELECT * FROM characters")
+    return cur.fetchall()
+
+def get_character():
+    name = input('Please enter the name of the character: ').lower()
+
+    cur.execute("SELECT * FROM characters WHERE name = :name", {'name': name})
+    return cur.fetchone()
+
+
+# Start program
+
+
+print("Welcome to The Lord of the Rings Character Database!\n")
+
+print("What would you like to do? Please select a number: ")
+
+continue_program = True
+
+while continue_program:
+    selection = input("""
+        1. View all the characters.
+        2. Find character by name.
+        3. Delete a character.
+        4. Add a character.
+        5. Change a characters life status.
+        6. Exit.
+    """)
+
+    if (selection == '1'):
+        characters = get_characters()
+        index = 1
+        for character in characters:
+            print(f"{index}) Name: {character[0]}, Race: {character[1]}, Life Status: {character[2]}, Image Link: {character[3]}")
+            index += 1
+    elif (selection == '2'):
+        character = get_character()
+        print(f"""
+            Name: {character[0]}
+            Race: {character[1]}
+            Life Status: {character[2]}
+            Image Link: {character[3]}
+        """)
+    elif (selection == '3'):
+        remove_character()
+    elif (selection == '4'):
+        add_character()
+    elif (selection == '5'):
+        update_character_life_status()
+    elif (selection == '6'):
+        print('Come again soon!!')
+        continue_program = False
+    else:
+        print('Please ensure that your selection is a number between 1 and 5')
